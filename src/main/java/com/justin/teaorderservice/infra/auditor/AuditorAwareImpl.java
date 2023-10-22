39f4914bf29594c28958bb409b9bc1d0bac8b88b
seojeonghyeon <seojeonghyeon0630@gmail.com>
2023-10-22 17:39:23 +0900

    Update AuditorAwareImpl.java
    Session 확인하는 부분 삭제

--- src/main/java/com/justin/teaorderservice/infra/auditor/AuditorAwareImpl.java
+++ src/main/java/com/justin/teaorderservice/infra/auditor/AuditorAwareImpl.java
@@ -1,18 +1,10 @@
 package com.justin.teaorderservice.infra.auditor;
 
-import com.justin.teaorderservice.infra.session.SessionConst;
 import com.justin.teaorderservice.modules.member.Member;
 import com.justin.teaorderservice.modules.member.MemberAdapter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.Objects;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
@@ -23,23 +15,12 @@ public class AuditorAwareImpl implements AuditorAware<String> {
             return getAuthentication(authentication);
         }
 
-        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-        HttpSession session = request.getSession();
-        if(null != session.getAttribute(SessionConst.LOGIN_MEMBER)){
-            return getSession(session);
-        }
-
         return null;
     }
 
     private Optional<String> getAuthentication(Authentication authentication){
         MemberAdapter memberAdapter = (MemberAdapter) authentication.getPrincipal();
         Member member = memberAdapter.getMember();
-        return Optional.of(member.getId());
-    }
-
-    private Optional<String> getSession(HttpSession session){
-        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         return Optional.of(member.getId());
     }
 }
