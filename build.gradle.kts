b8b1efc0edeca4869c00377b63729cfb8bdb2b3c
seojeonghyeon <seojeonghyeon0630@gmail.com>
2024-03-02 16:29:08 +0900

    임시 저장

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,22 @@
 plugins {
+    val kotlinVersion = "1.9.20"
+
     java
     id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
+<<<<<<< Updated upstream
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.8.22"
     id("org.jetbrains.kotlin.plugin.spring") version "1.8.22"
     id("org.jetbrains.kotlin.kapt") version "1.8.22"
+=======
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("plugin.lombok") version kotlinVersion
+    id("io.freefair.lombok") version "8.1.0"
+>>>>>>> Stashed changes
 }
 
 group = "me.justin"
@@ -26,6 +37,9 @@ repositories {
 }
 
 dependencies {
+    val mockkVersion = "1.13.8"
+    val querydslVersion = "5.0.0"
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -47,11 +61,12 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("org.modelmapper:modelmapper:2.4.5")
-    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-jpa:${querydslVersion}:jakarta")
+    kapt("com.querydsl:querydsl-apt:${querydslVersion}:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:${querydslVersion}:jakarta")
     annotationProcessor("jakarta.annotation:jakarta.annotation-api")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
-    implementation("com.querydsl:querydsl-core:5.0.0")
+    implementation("com.querydsl:querydsl-core:${querydslVersion}")
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("jakarta.annotation:jakarta.annotation-api")
     implementation("jakarta.persistence:jakarta.persistence-api")
@@ -60,9 +75,16 @@ dependencies {
     implementation("org.codehaus.janino:janino:3.1.2")
     annotationProcessor("org.projectlombok:lombok")
 
+<<<<<<< Updated upstream
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+=======
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+>>>>>>> Stashed changes
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
@@ -73,6 +95,7 @@ dependencies {
     testImplementation("com.tngtech.archunit:archunit-junit5:0.13.1")
     testImplementation("org.testcontainers:junit-jupiter:1.13.0")
     testAnnotationProcessor("org.projectlombok:lombok")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
 }
 
 val generated = "src.main/generated"
@@ -89,7 +112,27 @@ sourceSets{
     }
 }
 
+kapt {
+    keepJavacAnnotationProcessors = true
+}
+
 tasks{
+
+    test {
+        useJUnitPlatform()
+    }
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
     "clean" {
         doLast{
             file("generated").deleteRecursively()
